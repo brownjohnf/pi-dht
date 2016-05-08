2f2b9b8c2f7a9fecc6a60ee7cf216a1b7f2cef91
John (Jack) Brown <jack@brownjohnf.com>
2016-05-08 16:43:15 -0700

    Log to sparkfun data

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 var sensorLib = require('node-dht-sensor');
+var request   = require('request');
 
 var sensor = {
     initialize: function () {
         return sensorLib.initialize(22, 4);
     },
     read: function () {
-        var readout = sensorLib.read();
-        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
-            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        var readout = sensorLib.read(),
+            url,
+            temp,
+            humidity;
+
+        temp = readout.temperature.toFixed(2);
+        humidity = readout.humidity.toFixed(2);
+
+        console.log('Temperature: ' + temp + 'C, ' + 'humidity: ' + humidity + '%');
+
+        url = 'https://data.sparkfun.com/input/xRxozvJKz8tpV1z49bvw?private_key=ZaNk7zYD71s6kY9WaRrm&humidity=' +
+          humidity +
+          '&temp=' +
+          temp;
+
+        request(url, function(err, res, body) {
+          console.log(err, res, body);
+        });
+
         setTimeout(function () {
             sensor.read();
         }, 2000);
